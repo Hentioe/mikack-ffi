ea6f04445dd090e982b3ce3050fd1cf2123e6768
Hentioe <me@bluerain.io>
2020-02-24 21:58:31 -0400

    Add index/search api

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
-use libc::{c_char, c_int, size_t};
+use libc::{c_char, c_int, c_uint, size_t};
 use mikack::{extractors, models};
-use std::{collections::HashMap, ffi::CString, mem, slice};
+use std::{
+    collections::HashMap,
+    ffi::{CStr, CString},
+    mem, slice,
+};
 
 #[derive(Debug)]
 #[repr(C)]
@@ -130,4 +134,87 @@ pub extern "C" fn find_platforms(
     let platforms = CArray::from(&extractors::find_platforms(includes, excludes));
 
     Box::into_raw(Box::new(platforms))
+}
+
+type ExtrPtr = *const dyn extractors::Extractor;
+
+#[no_mangle]
+pub extern "C" fn get_extr(domain_ptr: *mut c_char) -> *mut ExtrPtr {
+    let domain = unsafe {
+        let cstr = CStr::from_ptr(domain_ptr);
+        cstr.to_str().unwrap()
+    };
+
+    Box::into_raw(Box::new(
+        extractors::get_extr(domain).unwrap().as_ref() as ExtrPtr
+    ))
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct Comic {
+    title: *mut c_char,
+    url: *mut c_char,
+    cover: *mut c_char,
+}
+
+impl From<&models::Comic> for Comic {
+    fn from(c: &models::Comic) -> Self {
+        Comic {
+            title: CString::new(c.title.as_bytes()).unwrap().into_raw(),
+            url: CString::new(c.url.as_bytes()).unwrap().into_raw(),
+            cover: CString::new(c.cover.as_bytes()).unwrap().into_raw(),
+        }
+    }
+}
+
+impl From<Vec<models::Comic>> for CArray<Comic> {
+    fn from(list: Vec<models::Comic>) -> Self {
+        let len = list.len();
+        let items = list.iter().map(|c| Comic::from(c)).collect::<Vec<_>>();
+
+        let mut boxed_data = items.into_boxed_slice();
+        let data = boxed_data.as_mut_ptr();
+        mem::forget(boxed_data);
+
+        CArray { len, data }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn index(extr_ptr_ptr: *mut ExtrPtr, page: c_uint) -> *mut CArray<Comic> {
+    let ptr = unsafe { Box::from_raw(extr_ptr_ptr) };
+    let extr = unsafe { &**ptr };
+
+    let array = CArray::from(extr.index(page).unwrap());
+    Box::into_raw(Box::new(array))
+}
+
+#[no_mangle]
+pub extern "C" fn free_comic_array(ptr: *mut CArray<Comic>) {
+    unsafe {
+        let array = Box::from_raw(ptr);
+        slice::from_raw_parts_mut(array.data, array.len)
+            .iter_mut()
+            .map(|c: &mut Comic| {
+                CString::from_raw(c.title);
+                CString::from_raw(c.url);
+                CString::from_raw(c.cover);
+            })
+            .for_each(drop);
+        mem::drop(Box::from_raw(array.data));
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn search(
+    extr_ptr_ptr: *mut ExtrPtr,
+    keywords_ptr: *const c_char,
+) -> *mut CArray<Comic> {
+    let ptr = unsafe { Box::from_raw(extr_ptr_ptr) };
+    let extr = unsafe { &**ptr };
+    let keywords = unsafe { CStr::from_ptr(keywords_ptr).to_str().unwrap() };
+
+    let array = CArray::from(extr.search(keywords).unwrap());
+    Box::into_raw(Box::new(array))
 }
